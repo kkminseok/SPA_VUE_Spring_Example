8d8d402e78fd83736f0cd9799194f9ea830f477d
kms <kms@kmsui-MacBookPro.local>
2022-03-09 00:16:54 +0900

    3_13 event 추가

---
+++ Ch3_Vuejs3/3_13Vue_event/src/App.vue
@@ -0,0 +1,164 @@
+<template>
+  <form v-on:submit.prevent="onSubmit">
+    <table>
+      <tr>
+        <td>user Id</td>
+        <td>
+          <input tpye="text" v-on:input="userId = $event.target.value" v-bind:value="userId">
+        </td>
+      </tr>
+      <tr>
+        <td>password</td>
+        <td>
+          <input type="password" v-on:change="userPw = $event.target.value" v-bind:value="userPw">
+        </td>
+      </tr>
+      <tr>
+        <td>introduce</td>
+        <td><textarea rows="5" v-model="introduction"></textarea></td>
+      </tr>
+      <tr>
+        <td>developer ? </td>
+        <td>
+          <input type="checkbox" v-on:change="handleCheckBox">
+        </td>
+      </tr>
+      <tr>
+        <td>developer ? </td>
+        <td>
+          <input type="checkbox" v-on:change="handleCheckBox()">
+        </td>
+      </tr>
+      <tr>
+        <td>developer ? </td>
+        <td>
+          <input type="checkbox" v-on:change="handleCheckBox2">
+        </td>
+      </tr>
+      <tr>
+        <td>developer ? </td>
+        <td>
+          <input type="checkbox" v-on:change="handleCheckBox2($event)">
+        </td>
+      </tr>
+      <tr>
+        <td>developer ? </td>
+        <td>
+          <input type="checkbox" v-on:change="handleCheckBox3('check!!!')">
+        </td>
+      </tr>
+      <tr>
+        <td>forigner ? </td>
+        <td>
+          <input type="checkbox" v-on:change="handleCheckBox4('check!!',$event)" true-value="yes" false-value="no">
+        </td>
+      </tr>
+      <tr>
+        <td>hobby</td>
+        <td>
+          <input type="checkbox" id="chkSports" value="Sports" v-model="hobbyList">
+          <label for="chkSports">Sports</label>
+          <input type="checkbox" id="chkMusic" value="Music" v-model="hobbyList">
+          <label for="chkMusic">Music</label>
+          <input type="checkbox" id="chkMovie" value="Movie" v-model="hobbyList">
+          <label for="chkMovie">Movie</label>
+        </td>
+      </tr>
+      <tr>
+        <td>gender</td>
+        <td>
+          <input type="radio" id="rdoMale" value="male" v-model="gender">
+          <label for="rdoMale">Male</label>
+          <input type="radio" id="rdoFemale" value="female" v-model="gender">
+          <label for="rdoFemale">Female</label>
+        </td>
+      </tr>
+      <tr>
+        <td>area</td>
+        <select v-model="nationality">
+          <option disabled value="">Please select one</option>
+          <option value="01">Korea</option>
+          <option value="02">Germany</option>
+          <option value="03">Australia</option>
+        </select>
+      </tr>
+      <tr>
+        <td>have car</td>
+        <td>
+          <select v-model="carList" multiple>
+            <option>Volvo</option>
+            <option>Saab</option>
+            <option>Opel</option>
+          </select>
+        </td>
+      </tr>
+    </table>
+    <p>
+      <button type="submit">submit</button>
+    </p>
+  </form>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default{
+  setup(){
+    const userId = ref("")
+    const userPw = ref("")
+    const introduction = ref("")
+    const developer = ref(false)
+    const foreigner = ref("no")
+    const hobbyList = ref([])
+    const gender = ref('male')
+    const nationality = ref("")
+    const carList = ref([])
+
+    const onSubmit = () => {
+      console.log('userId : ',userId.value)
+      console.log('userPw : ',userPw.value)
+      console.log('introduction : ',introduction.value)
+      console.log('developer : ',developer.value)
+      console.log('foreigner : ',foreigner.value)
+      console.log('hobbyList : ',hobbyList.value)
+      console.log('gender : ',gender.value)
+      console.log('nationality : ',nationality.value)
+      console.log('carList : ',carList.value)
+    }
+
+    const handleCheckBox = () =>{
+      console.log('handleCheckBox')
+    }
+
+    const handleCheckBox2 = (event) =>{
+      console.log('handleCheckBox2 event : ', event)
+    }
+
+    const handleCheckBox3 = (message) =>{
+      console.log('handleCheckBox3 message : ',message)
+    }
+
+    const handleCheckBox4 = (message,event) =>{
+      console.log('handleCheckBox4 message : ',message)
+      console.log('handleCheckBox4 event : ',event)
+    }
+
+    return {
+      userId,
+      userPw,
+      introduction,
+      developer,
+      foreigner,
+      hobbyList,
+      gender,
+      nationality,
+      carList,
+      onSubmit,
+      handleCheckBox,
+      handleCheckBox2,
+      handleCheckBox3,
+      handleCheckBox4
+    }
+  }
+}
+</script>
