7714e520aade9ef1a7057be2ed53a9a5aecf135d
kms <kms@kmsui-MacBookPro.local>
2022-03-08 14:47:15 +0900

    3_11 watch 추가

---
+++ Ch3_Vuejs3/3_11VueWatch/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  mainboard Price : <input type="text" v-on:input="price = $event.target.value" v-bind:value ="price">
+  
+</template>
+
+<script>
+import {ref, watch} from 'vue';
+
+export default{
+  setup(){
+    const price = ref(91273)
+
+    watch(
+      price,
+      (cur,prev) =>{
+        console.log("변경되기 전 : ",prev)
+        console.log("변경된 후 : ",cur);
+        },
+        {immediate: true},
+    )
+
+    return {
+      price
+    }
+  }
+}
+</script>
